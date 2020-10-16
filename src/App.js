7f8aa8c6c727fa4875c55221641845fd2a2a2b08
Savannah Vaithilingam <savannah.vaithilingam@qa.com>
2020-10-16 11:46:11 +0100

    Initial project structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyInfo from './components/MyInfo';
+
+function App() {
+  return (
+    <div>
+      <p>hello friends</p>
+      <MyInfo/>
+    </div>
+  );
+}
+
+export default App;
